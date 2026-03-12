1fc1eaf602778e5d4e534f64c617b5f1aa8bfc4f
DistroZLeon <alystinga@yahoo.ro>
2026-03-12 18:03:39 +0000

    [FEAT] Added necessary Transport & Crypto Utils

---
+++ transport.py
@@ -0,0 +1,75 @@
+import struct
+import os
+import math
+
+class Packet:
+
+    # Protocol Flags
+    SYN= 0x01   # Handshake
+    ACK= 0x02   # Acknowledge received data
+    FIN= 0x04   # End connection
+    DAT= 0x08   # Contains data
+
+    def __init__(self, ack_num: int, flags: int, data: bytes= b''):
+        self.ack_num= ack_num
+        self.flags= flags
+        self.data= data
+
+    def pack(self)-> bytes:
+        header= struct.pack(">IB", self.ack_num, self.flags)
+        return header+ self.data
+
+    @classmethod
+    def unpack(cls, raw_data: bytes):
+        if len(raw_data)< 5:
+            raise ValueError("Payload is to short to contain a valid header!")
+
+        ack_num, flags= struct.unpack(">IB", raw_data[:5])
+        data= raw_data[5:]
+
+        return cls(ack_num, flags, data)
+
+    def has_flag(self, flag: int)-> bool:
+        return (self.flags& flag)!= 0
+
+    def __str__(self):
+        current_flags=[]
+        if self.has_flag(self.SYN): current_flags.append("SYN")
+        if self.has_flag(self.ACK): current_flags.append("ACK")
+        if self.has_flag(self.FIN): current_flags.append("FIN")
+        if self.has_flag(self.DAT): current_flags.append("DAT")
+
+        return f"[Packet | ACK: {self.ack_num} | FLAGS: {'+'.join(current_flags)} | Payload: {len(self.data)} bytes]"
+
+class Fragmenter:
+    UPSTREAM_SIZE= 110
+    DOWNSTREAM_SIZE= 200
+
+    @staticmethod
+    def read_chunk(filepath: str, seq_num: int, chunk_size: int)-> bytes:
+        if not os.path.exists(filepath):
+            raise FileNotFoundError(f"File not found: {filepath}!")
+
+        offset= (seq_num- 1)* chunk_size
+
+        with open(filepath, "rb") as f:
+            f.seek(offset)
+            data= f.read(chunk_size)
+            return data
+
+    @staticmethod
+    def write_chunk(filepath: str, data: bytes):
+        with open(filepath, "ab") as f:
+            f.write(data)
+
+    @staticmethod
+    def get_total_chunks(filepath: str, chunk_size: int)-> int:
+        if not os.path.exists(filepath):
+            return 0
+        file_size= os.path.getsize(filepath)
+        return math.ceil(file_size/ chunk_size)
+
+    @staticmethod
+    def init_empty(filepath: str):
+        with open(filepath, "wb") as f:
+            pass
